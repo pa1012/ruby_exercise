f1a0099d6b3d5b1d38b9fe51803380a0977050bb
pa1012 <phuonganht1012@gmail.com>
2021-08-27 17:09:58 +0700

    caesar_cipher Add test

---
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -0,0 +1,26 @@
+class CaesarCipher
+  def translate(string, shift)
+    char = string.split("")
+    char = char.map do |c| 
+      if ('a'..'z').include?(c) 
+        ((c.ord + shift - 97) % 26 + 97).chr
+      elsif ('A'..'Z'). include?(c)
+        ((c.ord + shift - 65) % 26 + 65).chr
+      else
+         c
+      end
+    end
+    
+    
+    return char.join()
+  end
+end
+
+caesar_cipher = CaesarCipher.new()
+
+p caesar_cipher.translate("What a string!", 5)
+p caesar_cipher.translate("Zz", 5)
+p caesar_cipher.translate("Ee", -5)
+p caesar_cipher.translate("Zz", 83)
+p caesar_cipher.translate("Ee", -83)
+
